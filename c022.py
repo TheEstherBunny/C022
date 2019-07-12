3da5272a024116482f22fb414292fa788137d377
Esthy Hung <esther.hung@stcatz.ox.ac.uk>
2019-07-12 11:46:38 +0100

    Add existing file

---
+++ c022.py
@@ -0,0 +1,12 @@
+
+
+
+"""solutions of Schrodinger’s equation by numerical integration
+
+
+find numerical solutions to the time-independent Schrodinger equation for ¨
+the quantum harmonic oscillator. This problem illustrates the solution of a stiff differential equation.
+
+we can try boundary conditions x_0 = 0 and x_1 = 23402394
+
+"""
